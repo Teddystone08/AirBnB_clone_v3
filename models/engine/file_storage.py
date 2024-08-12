c10cfc0f9106359b5dce19e184be59fba634cdcc
Teddystone08 <ernestlrty@hotmail.com>
2024-08-12 21:57:18 +0000

    *

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """
 Contains the FileStorage class
 """
-
+import models
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -68,3 +68,35 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+
+          def get(self, cls, id):
+        """A method to retrieve one object:
+        Returns the object based on the class & its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_objs_dict = models.storage.all(cls)
+        for value in all_objs_dict.values():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        A count method for number of objects in storage:
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
